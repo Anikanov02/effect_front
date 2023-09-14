310de8f56430e51757dae7ed793f743987736c85
Kristex95 <kristex95@gmail.com>
2023-09-14 16:54:33 +0300

    test page for youtube metadata created

---
+++ pages/test/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <img :src="img_url" alt="">
+      <p>{{ title }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: "Hello, Vue!",
+        img_url: "",
+        title: ""
+      };
+    },
+    mounted(){
+        const vidurl = 'https://www.youtube.com/watch?v=ZvUMvV_YZKg';
+
+        fetch(`https://noembed.com/embed?dataType=json&url=${vidurl}`)
+        .then(res => res.json())
+        .then(data => {
+            this.img_url = data.thumbnail_url
+            this.title = data.title
+        })
+    },
+    methods: {
+      changeMessage() {
+        this.message = "New Message!";
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  </style>
+  
